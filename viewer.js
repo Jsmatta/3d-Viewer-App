a7410891ed101ec6235ad0797e557d6585b04210
Jaiveer <jaiveermatta1@gmail.com>
2025-05-18 17:49:54 -0230

    basic orbit controls added

--- viewer.js
+++ viewer.js
@@ -80,3 +80,12 @@ document.getElementById('reset').addEventListener('click', () => {
 );
 
 // Orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.screenSpacePanning = false;
+controls.maxPolarAngle = Math.PI / 2;
+controls.minDistance = 0.5;
+controls.maxDistance = 5;
+controls.target.set(0, 0, 0);
+controls.update();
